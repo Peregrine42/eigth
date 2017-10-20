8adbd043fb7cbc3700fd7e9b280e684ca280dd85
Duncan Fermor <duncan_fermor@fastmail.com>
2017-10-20 19:27:41 +0100

    style...

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,13 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   alias_attribute :name, :username
   # validate :cannot_set_your_own_role
+  validate :username_must_be_all_lowercase_with_underscores
+
+  def username_must_be_all_lowercase_with_underscores
+    unless username.match(/^[0-9a-z_]+$/)
+      errors.add(:username, "contains non-lowercase letters, or other symbols")
+    end
+  end
 
   def cannot_set_your_own_role
     !((self.role != self.role_was) && (@current_user.id == self.id))
